ac175457ffdfa02ed4112985d8743c7077c1d777
302xdwill <davidom302@gmail.com>
2025-04-21 01:03:02 -0500

    new estudiante

--- ms_estudiante/src/main/java/com/upeu/ms_estudiante/util/EstudianteSeeder.java
+++ ms_estudiante/src/main/java/com/upeu/ms_estudiante/util/EstudianteSeeder.java
@@ -25,8 +25,8 @@ public class EstudianteSeeder implements CommandLineRunner {
                 Estudiante estudiante = new Estudiante();
                 estudiante.setNombre("Nombre" + i);
                 estudiante.setApellido("Apellido" + i);
-                estudiante.setDni(String.format("1234567%02d", i)); // DNI simulado de 8 dígitos
-                estudiante.setCodigo("2024" + String.format("%03d", i)); // Código simulado tipo "2024001"
+                estudiante.setDni(String.format("1234567%02d", i)); // DNI de 8 dígitos
+                estudiante.setCodigo("2024" + String.format("%03d", i)); // Código tipo "2024001"
                 estudiante.setCorreo("estudiante" + i + "@upeu.edu.pe");
                 estudiante.setTelefono("9000000" + String.format("%02d", i));
 
